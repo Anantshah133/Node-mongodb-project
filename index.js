ff0dfd9109c35802496a677d40a8d27893db66a5
Anantshah133 <anantshah130305@gmail.com>
2024-03-02 23:27:50 +0530

    File upload using multer done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const upload = require("./src/utils/multer");
 const { connectDB } = require("./src/config/db");
 const { insertData } = require("./src/crud/operations");
 const PORT = 5000;
+
 connectDB();
 
 app.set('view engine', 'ejs');
@@ -18,16 +20,22 @@ app.get('/', (req, res) => {
 app.get('/marksInfo', (req, res) => {
     res.render("marksInfo.ejs");
 })
+
 app.get('/table-marks', (req, res) => {
     res.render("table.ejs");
 })
+
 app.get('/student-card', (req, res) => {
     res.render("card.ejs");
 })
 
-app.post('/goNext', async (req, res) => {
+app.post('/marks-submit', (req, res)=>{
+    res.redirect("/student-card");
+})
+
+app.post('/goNext', upload.single("studentImg"), async (req, res) => {
     try {
-        let result = await insertData(req.body);
+        let result = await insertData({...req.body, studentImg: req.file.filename});
         if(result){
             res.redirect('/table-marks');
         }
